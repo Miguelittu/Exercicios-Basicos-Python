dcec5c053cdea28c48eff895833fc33b9c7b6faa
Miguelittu <162479587+Miguelittu@users.noreply.github.com>
2024-03-06 13:47:20 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,11 @@
+print( "BEM VINDO A CALCULADORA TUDO E TODOS!" )
+n1 = int(input ("PRIMEIRO VALOR: "))
+n2 = int(input ("SEGUNDO VALOR: "))
+so = n1 + n2
+su = n1 - n2
+mul = n1 * n2 
+d = n1 / n2  
+print ("SOMA =", so)
+print ("SUBTRAÇÃO =", su)
+print ("MULTIPLICAÇÃO =",  mul)
+print ("DIVISÃO =",  d)
